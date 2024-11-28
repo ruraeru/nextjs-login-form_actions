d5cec99dda937b18ea868553d6538b6cd7aefe67
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-28 10:13:14 +0900

    prisma init & add model user, tweet, like

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ export const PASSWORD_MIN_LENGTH = 10;
 export const USERNAME_MIN_LENGTH = 5;
 
 export const EMAIL_REGEX = new RegExp("@zod.com");
-export const PASSWORD_REGEX = new RegExp("^[A-Za-z]+[0-9]+$");
+export const PASSWORD_REGEX = new RegExp("/^(?=.*d).{10,}$/");
